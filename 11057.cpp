6cea72ce5a40c3868aee1a658684aeebe24d6e13
yeonui-0626 <73818604+yeonui-0626@users.noreply.github.com>
2021-01-15 23:20:32 +0900

    Add files via upload

---
+++ 11057.cpp
@@ -0,0 +1,49 @@
+
+
+/*
+ 백준 11057 오르막수
+
+ 시간초과
+
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int n;
+int cnt=0;
+int result;
+
+void dfs(vector<int>& v,int prev) {
+	if (v.size() == n) {
+		cnt++;
+		/*
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i];
+		}
+		cout << '\n';
+		*/
+		return;
+	}
+	else {
+		for (int i = prev; i <= 9; i++) {
+			v.push_back(i);
+			dfs(v, i);
+			v.pop_back();
+		}
+	}
+}
+
+int main() {
+	cin >> n;
+	vector<int> v;
+	dfs(v, 0);
+	
+	result = cnt % 10007;
+
+	cout << result;
+
+	return 0;
+}
